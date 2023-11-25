a947d478af4a6b78fe265383191318728b2f9700
Rushikeshsasamkar <44942941+Rushikeshsasamkar@users.noreply.github.com>
2023-11-25 22:04:03 +0530

    Error handling

--- src/App.js
+++ src/App.js
@@ -4,12 +4,28 @@ import  ComponentA  from './ComponentA';
 import TimerOne from './Timer/TimerOne';
 
 class App extends React.Component {
+
+  constructor(){
+
+    super();
+    this.state={
+      timerOn:false
+    }
+
+  }
+
+  handleTimerOn=()=>{
+    this.setState((prevState)=>({timerOn:!prevState.timerOn}))
+    
+  }
   render(){
   return (
     <div className="App">
-        
-        {/* <ComponentA/> */}
-        <TimerOne/>
+        <TimerOne timerOn={this.state.timerOn}/>
+        <button onClick={this.handleTimerOn}>
+          {this.state.timerOn?"Stop":"Start"}
+        </button>
+
     </div>
   );
 }
--- src/Timer/TimerOne.js
+++ src/Timer/TimerOne.js
@@ -1,59 +1,69 @@
 import React from "react";
 
-export default class TimerOne extends React.Component{
+export default class TimerOne extends React.Component {
+  constructor() {
+    super();
 
-    constructor(){
-        super();
-        this.state={
-            time:0
+    this.state = {
+      time: 0
+    };
+
+    this.timer = null;
+  }
+
+
+  componentDidMount() {
+    console.log("Timer ComponentDidMount");
+    console.log("_________________________________");
+    
+    
+  }
+
+  getSnapshotBeforeUpdate(prevProp, prevState) {
+    console.log("Timer getSnapshotBeforeUpdate");
+    console.log("_________________________________");
+    return null;
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("Timer shouldComponentUpdate");
+    console.log("_________________________________");
+    return true;
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("Timer componentDidUpdate");
+    console.log("_________________________________");
+
+    if(prevProps.timerOn !== this.props.timerOn){
+        if(this.props.timerOn){
+            this.timer = setInterval(()=>{
+            this.setState((prevState)=>({time: prevState.time+1}))
+            },1000)
         }
-        this.timer=null;
-        console.log("TimeOne Constructor");
+        else{
+            clearInterval(this.timer);
+        }
     }
 
-    static getDerivedStateFromProps(){
-        console.log("TimerOne getDerivedFromProps");
+    this.timer = setInterval(() => {
+      this.setState((prevState) => ({ time: prevState.time + 1 }));
+    }, 1000);
+  }
 
+  componentWillUnmount() {
+    console.log("Timer componentWillUnmount");
+    if (this.state.time === 10) {
+      clearInterval(this.timer);
     }
-    shouldComponentUpdate(){
-        return true;
-    }
+  }
 
-    // handlIncrease =()=>{
-    //     this.setState((prevState)=>{
-    //         return {count:prevState.count+1}
-    //     })
-    // }
-
-    render(){
-        return(<>
-            {/* console.log("TimerOne Render"); */}
-            <h1>Time spent</h1>
-            {new Date(this.state.time*1000).toISOString().slice(11,19)}
-
-            </>
-        )
-    }
-
-    componentDidMount(){
-        console.log("TimerOne componentDidMount");
-
-
-        this.timer=setInterval(()=>{
-            this.setState((prevState)=>({time:prevState.time+0.5}))
-        },1000);
-        
-    }
-
-    getSnapshotBeforeUpdate(prevProps, prevState){
-
-        console.log("TimerOne getSnapShotBeforeUpdate");
-        return null;
-
-    }
-
-    componentDidUpdate(){
-        console.log("TimerOne componetDidUpdate")
-    }
-
+  render() {
+    return (
+      <div>
+        <h2>Time Spent: {this.state.time}</h2>
+        {new Date(this.state.time * 1000).toISOString().slice(11, 19)}
+      </div>
+    );
+  }
 }
